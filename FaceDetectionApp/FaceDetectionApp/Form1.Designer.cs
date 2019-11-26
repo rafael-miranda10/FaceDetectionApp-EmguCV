77296b7096dda08efa4821e0ea689006fdea8af5
Rafael Arthur Rocha Miranda <43150739+rafael-miranda10@users.noreply.github.com>
2019-11-25 21:11:13 -0300

    [DEV] inicial

---
+++ FaceDetectionApp/FaceDetectionApp/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace FaceDetectionApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDetect = new System.Windows.Forms.Button();
+            this.pic = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnDetect
+            // 
+            this.btnDetect.Location = new System.Drawing.Point(713, 12);
+            this.btnDetect.Name = "btnDetect";
+            this.btnDetect.Size = new System.Drawing.Size(99, 41);
+            this.btnDetect.TabIndex = 0;
+            this.btnDetect.Text = "&Detect";
+            this.btnDetect.UseVisualStyleBackColor = true;
+            this.btnDetect.Click += new System.EventHandler(this.btnDetect_Click);
+            // 
+            // pic
+            // 
+            this.pic.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pic.Location = new System.Drawing.Point(12, 12);
+            this.pic.Name = "pic";
+            this.pic.Size = new System.Drawing.Size(695, 426);
+            this.pic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic.TabIndex = 1;
+            this.pic.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(821, 450);
+            this.Controls.Add(this.pic);
+            this.Controls.Add(this.btnDetect);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Face Detection App";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDetect;
+        private System.Windows.Forms.PictureBox pic;
+    }
+}
+
---
+++ FaceDetectionApp/FaceDetectionApp/Form1.cs
@@ -0,0 +1,48 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FaceDetectionApp
+{
+    public partial class Form1 : Form
+    {
+        static readonly CascadeClassifier cascadeClassifier = new CascadeClassifier("haarcascade_frontalface_alt_tree.xml");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDetect_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog() { Filter = "JPEG|*.jpg" })
+            {
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    pic.Image = Image.FromFile(openFileDialog.FileName);
+                    Bitmap img = new Bitmap(pic.Image);
+                    Image<Bgr, byte> grayImage = new Image<Bgr, byte>(img);
+                    Rectangle[] rectangles = cascadeClassifier.DetectMultiScale(grayImage, 1.4, 0);
+                    foreach (Rectangle rectangle in rectangles)
+                    {
+                        using (Graphics gr = Graphics.FromImage(img))
+                        {
+                            using (Pen pen = new Pen(Color.Red, 1))
+                            {
+                                gr.DrawRectangle(pen, rectangle);
+                            }
+                        }
+                    }
+                    pic.Image = img;
+                }
+            }
+        }
+    }
+}
